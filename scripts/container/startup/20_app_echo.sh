ee5a43f7b51e882a6b16bc205ad092cb4209844c
Teriyakidactyl <gray.jason@gmail.com>
2025-04-21 19:04:37 -0700

    update 1 file and create 1 file

---
+++ scripts/container/startup/20_app_echo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Display server configuration
+log "+----------------------------------+"
+log "SERVER_NAME: $SERVER_NAME"
+log "SERVER_PASS: $SERVER_PASS"
+log "WORLD_NAME: $WORLD_NAME"
+log "SERVER_PUBLIC: $SERVER_PUBLIC"
+log "SERVER_ARGS: $SERVER_ARGS"
+log "+----------------------------------+"
+sleep 1
